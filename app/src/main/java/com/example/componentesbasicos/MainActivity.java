b60480cd816ebf9bd3dc19ae75d6c995e48e93d5
lourivanluz <lourivan-rodrigues@hotmail.com>
2020-11-09 23:16:50 -0300

    att 1.02

--- app/src/main/java/com/example/componentesbasicos/MainActivity.java
+++ app/src/main/java/com/example/componentesbasicos/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.core.content.res.TypedArrayUtils;
 
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -15,21 +16,11 @@ import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
     private TextInputEditText tiPrincipioAtivo;
-    private EditText etPosologia;
-    private EditText etPeso;
-    private EditText etconcen;
-    private RadioButton rbPassaro;
-    private RadioButton rbNPassaro;
-    private RadioButton rbReptil;
-    private RadioButton rbMaMetaAlto;
-    private RadioButton rbMaMetabaixo;
-    private RadioButton rbKg;
-    private RadioButton rbg;
-    private RadioGroup rg_grupo;
-    private RadioGroup rge_g1;
-    private RadioGroup rge_g2;
-    private EditText etNomeCom;
-
+    private EditText etPeso, etconcenta, etPosologia, etNomeCom, etEsp;
+    private RadioButton rbPassaro, rbNPassaro, rbReptil, rbMaMetaAlto, rbMaMetabaixo, rbKg, rbg;
+    private RadioGroup rge_g1, rge_g2;
+    private TextView tv_alvo;
+    private boolean dadosValidados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         etPosologia = findViewById(R.id.et_Posologia);
         etPeso = findViewById(R.id.et_peso);
         etNomeCom = findViewById(R.id.et_NomeCom);
-        etconcen = findViewById(R.id.et_concentra);
+        etconcenta = findViewById(R.id.et_concentra);
         rbPassaro = findViewById(R.id.rb_Passaro);
         rbNPassaro = findViewById(R.id.rb_NPassaro);
         rbReptil = findViewById(R.id.rb_Reptil);
@@ -48,13 +39,15 @@ public class MainActivity extends AppCompatActivity {
         rbMaMetabaixo = findViewById(R.id.rb_MaMetaBaixo);
         rbKg = findViewById(R.id.rb_kg);
         rbg = findViewById(R.id.rb_g);
-        rg_grupo = findViewById(R.id.rg_Grupo);
         rge_g1 = findViewById(R.id.rg_g1);
         rge_g2 = findViewById(R.id.rg_g2);
+        etEsp = findViewById(R.id.et_Esp);
+        tv_alvo = findViewById(R.id.tv_alvo);
 
         radioGrupConfig();
-
     }
+
+    // gambis pra melhorar o layout dos radiogroup, deve ter um jeito mais facil
     public void radioGrupConfig ( ){
 
         rge_g1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -75,89 +68,124 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
     });
-    } // gambis pra melhorar o layout dos radiogroup, deve ter um jeito mais facil
+    }
 
+    //validar formulario
+    public boolean validarDados (){
+        boolean resultado = false;
+        int kalvo = retornaKalvo();
+
+        if ((((!TextUtils.isEmpty(tiPrincipioAtivo.getText().toString())) && (!TextUtils.isEmpty(etPosologia.getText().toString()))) && (((!TextUtils.isEmpty(etPeso.getText().toString())) && (kalvo !=0))) && ((!TextUtils.isEmpty(etEsp.getText().toString()))))) {
+           //descobrir como limpar mensagem de erro.
+            resultado = true;
+        }else if (TextUtils.isEmpty(tiPrincipioAtivo.getText().toString())){
+            tiPrincipioAtivo.setError("campo obrigatorio");
+            tiPrincipioAtivo.requestFocus();
+
+        }else if (TextUtils.isEmpty(etPosologia.getText().toString())){
+            etPosologia.setError("campo obrigatorio");
+            etPosologia.requestFocus();
+            resultado = false;
+        } else if (TextUtils.isEmpty(etPeso.getText().toString())){
+            etPeso.setError("campo obrigatorio");
+            etPeso.requestFocus();
+            resultado = false;
+        }else if (TextUtils.isEmpty(etEsp.getText().toString())){
+            etEsp.setError("campo obrigatorio");
+            etEsp.requestFocus();
+            resultado = false;
+        }else if (kalvo==0){
+            tv_alvo.setError("campo obrigatorio");
+            tv_alvo.requestFocus();
+            resultado = false;
+        }
+        if ((!TextUtils.isEmpty(etconcenta.getText().toString())) && (TextUtils.isEmpty(etNomeCom.getText().toString()))){
+            etNomeCom.setError("selecione um nome comercial");
+            etNomeCom.requestFocus();
+            resultado = false;
+        }
+        return resultado;
+    }
+
+    // filtra e chama o calculo
     public void calcular(View view) {
-        String principioAtivo = tiPrincipioAtivo.getText().toString();
-        String nomeComercial = etNomeCom.getText().toString();
-        String concentra = etconcen.getText().toString();
-        int k = retornaK();
-        double peso = retornaPeso();
-        //int frequenciaModelo = 12; // puxado do bd que diz de quanto em quanto tempo, no caso a cada 12horas
-        String[] bdPrinAtiv = {"doxiciclina", "enrofloxacino", "carprofeno", "Ceftriaxona"}; // fake bd. a ideia é buscar no banco de dados por esses principios ativos
-        String[] bdNomeCom = {"doxvet", "enrodog", "carprocat", "dipirona", "doflex"};
-        String[] especie = {"cachorro", "gato", "pato", "tartaruga"};
-       // int posologia = 30;//fake posologia que vai ser puxada no bd. pode ter varias posologia com o mesmo tipo de medicamento, ex doxiciclina pra cachorro tem posologia 10 e pra pato 30
-        System.out.println("test o valor da constante é " + k);
-        System.out.println("test o valor do peso é " + peso);
-        // tentando tratar entrada pra buscar no bd obs, n deu certo pq so aceita o primeiro verdadeiro,
+        dadosValidados = validarDados();
+        if (dadosValidados) {
+            int kmodelo = Integer.parseInt(etEsp.getText().toString()); // fake kmodelo, puxado do bd com a tag especie ou selecionada manualmente.
+            int frequencia = 12; // fake frequencia puxada do bd com a tag da posologia. não é obrigatoria.
+            int valorKalvo = retornaKalvo();
+            double peso = retornaPeso();
+            double posologia = Double.parseDouble(etPosologia.getText().toString());
+            double concen;
 
+            // temporario, ate o bd retornar a concentraçao quando selecionar nome comercial
+            if (TextUtils.isEmpty(etconcenta.getText().toString())){
+                concen = 20;
+            }else {
+                concen = 30;
+            }
 
-        if ((principioAtivo.equals("") || principioAtivo == null) && (nomeComercial != "")) {
-            String chave;
-            chave = nomeComercial;
+            calculos calcular = new calculos();
+
+            if ((!TextUtils.isEmpty(etconcenta.getText().toString())) && (frequencia!=0)){
+                //conta base, frequencia e concentração
+                String resultado = calcular.calculo(valorKalvo,peso,posologia,kmodelo,frequencia,concen); // retornar algo
+                System.out.println("test " + resultado);
+
+            } else if ((!TextUtils.isEmpty(etconcenta.getText().toString())) && (frequencia ==0)){
+                //conta base e concentração
+                String resultado = calcular.calculo(valorKalvo,peso,posologia,kmodelo,concen);
+                System.out.println("test " + resultado);
+
+            } else if ((TextUtils.isEmpty(etconcenta.getText().toString())) && (frequencia!=0)){
+                //conta base e frequencia
+                String resultado = calcular.calculo(valorKalvo,peso,posologia,kmodelo,frequencia);
+                System.out.println("test " + resultado);
+
+            } else if ((TextUtils.isEmpty(etconcenta.getText().toString())) && (frequencia ==0)){
+                // conta base
+                String resultado = calcular.calculo(valorKalvo,peso,posologia,kmodelo);
+                System.out.println("test " + resultado);
+
+            }
+        }else{
+            System.out.println("test falta dados");
+        }
+    }
+
+    // n sei pq mas fiz um buscar no bd
+    public void buscarBdnomeCom(String[] bdNomeCom){
+       // String[] bdNomeCom = {"doxvet", "enrodog", "carprocat", "dipirona", "doflex"};fake bd
+        if (!TextUtils.isEmpty(etNomeCom.getText().toString())) {
+            String chave = etNomeCom.getText().toString();
             for (int num1 = 0; num1 < bdNomeCom.length; num1++) {
-                System.out.println("test buscando por " + chave);
-                if (chave.equals(bdNomeCom[num1])) { // buscando no array de bdRemedios
+                if (chave.equals(bdNomeCom[num1])) {
                     System.out.println("test importante igual " + bdNomeCom[num1] + " na casa: " + num1);
                     num1 = bdNomeCom.length + 1;
                 } else {
-                    System.out.println("test importante diferente");
+                    System.out.println("diff");
                 }
             }
-        } else if ((nomeComercial.equals("") || nomeComercial == null) || (principioAtivo != null && nomeComercial != null)) {
-            String chave;
-            chave = principioAtivo;
+        }
+    }
+    public void buscarBdPrinciAtiv (String[] bdPrinAtiv){
+       // String[] bdPrinAtiv = {"doxiciclina", "enrofloxacino", "carprofeno", "Ceftriaxona"};fake bd
+
+        if (!TextUtils.isEmpty(tiPrincipioAtivo.getText().toString())) {
+            String chave = tiPrincipioAtivo.getText().toString();
             for (int num1 = 0; num1 < bdPrinAtiv.length; num1++) {
-                System.out.println("test buscando por " + chave);
-                if (chave.equals(bdPrinAtiv[num1])) { // buscando no array de bdRemedios
+                if (chave.equals(bdPrinAtiv[num1])) {
                     System.out.println("test importante igual " + bdPrinAtiv[num1] + " na casa: " + num1);
                     num1 = bdPrinAtiv.length + 1;
                 } else {
-                    System.out.println("test importante diferente");
+                    System.out.println("diff");
                 }
             }
-        } else if ((principioAtivo.equals("") || principioAtivo == null) && (nomeComercial.equals("") || nomeComercial == null)) {
-            System.out.println("test Preencha Princio ativo ou nome comercial");
         }
-        if (nomeComercial != null && (concentra == "" || concentra == null)) { // n sei pq mas concentra tem algum valor
-                System.out.println("test Preencha a concentração" + concentra);
-        }
-        if (k == 0) {
-            System.out.println("test Selecione um animal alvo");
-            }
+    }
 
-        }
-
-        public void calculo (int k , double peso, double posologia, int concentra, int frequenciaModelo){
-
-            double tbmAlvo;
-            double tbmModelo;
-            double doseTotal;
-            double doseTotalMl;
-            double posologiaAlvo;
-            double frequenciaAlvo;
-            double tmeAlvo;
-            double tmeModelo;
-
-            tbmModelo = 70*(Math.pow(10,0.75)); //  metabolismo do modelo = cao de 10kg
-            tbmAlvo = k*(Math.pow(peso,0.75)); // metabolismo  alvo que quer medicar.
-            doseTotal = ((posologia*10)/tbmModelo)*tbmAlvo; // dose total para o animal alvo
-            posologiaAlvo = doseTotal/peso; // para saber a posologia do animal alvo caso queira colocar no BD.
-            tmeModelo = tbmModelo/10;
-            tmeAlvo = tbmAlvo/peso;
-
-            System.out.println("Test o animal alvo vai receber: "+ doseTotal + "mg");
-            System.out.println("test posologia para a animal alvo é: "+ posologiaAlvo );
-
-            System.out.println("Test usando a concentração de 20mg/ml");
-
-            frequenciaAlvo= (tmeModelo*frequenciaModelo)/tmeAlvo; // frequencia para medicação do amimal alvo
-            doseTotalMl = doseTotal/concentra;
-            System.out.println("Test vai receber: " + doseTotalMl + " ml");
-            System.out.println("test medicar a cada: "+ frequenciaAlvo );
-        }
-        public int retornaK (){
+    //retorna o k alvo
+    public int retornaKalvo (){
         int k;
         if (rbPassaro.isChecked()){
             k = 129;
@@ -178,8 +206,10 @@ public class MainActivity extends AppCompatActivity {
             k = 0;
             return k;
         }
-        }
-        public double retornaPeso (){
+    }
+
+    // retorna o peso
+    public double retornaPeso (){
         double peso = Double.parseDouble(etPeso.getText().toString());
         if (rbg.isChecked()){
             peso = peso/1000;
---
+++ app/src/main/java/com/example/componentesbasicos/calculos.java
@@ -0,0 +1,86 @@
+package com.example.componentesbasicos;
+
+public class calculos {
+
+
+    public String calculo (int Kalvo , double peso, double posologia, int kmodelo){
+        double tbmAlvo;
+        double tbmModelo;
+        double doseTotal;
+        double posologiaAlvo;
+
+
+        tbmModelo = kmodelo*(Math.pow(10,0.75)); //  metabolismo do modelo = cao de 10kg
+        tbmAlvo = Kalvo*(Math.pow(peso,0.75)); // metabolismo  alvo que quer medicar.
+        doseTotal = ((posologia*10)/tbmModelo)*tbmAlvo; // dose total para o animal alvo
+        posologiaAlvo = doseTotal/peso; // para saber a posologia do animal alvo caso queira colocar no BD.
+
+        //antes de converter preciso tratar pra usar so 2 numeros depois do ponto.
+        String resultado = "posologia é " + posologiaAlvo;
+        return resultado;
+    }
+    public String calculo (int Kalvo , double peso, double posologia, int kmodelo, int frequencia){
+
+        double tbmAlvo;
+        double tbmModelo;
+        double doseTotal;
+        double posologiaAlvo;
+        double frequenciaAlvo;
+        double tmeAlvo;
+        double tmeModelo;
+
+        tbmModelo = kmodelo*(Math.pow(10,0.75)); //  metabolismo do modelo = cao de 10kg
+        tbmAlvo = Kalvo*(Math.pow(peso,0.75)); // metabolismo  alvo que quer medicar.
+        doseTotal = ((posologia*10)/tbmModelo)*tbmAlvo; // dose total para o animal alvo
+        posologiaAlvo = doseTotal/peso; // para saber a posologia do animal alvo caso queira colocar no BD.
+        tmeModelo = tbmModelo/10;
+        tmeAlvo = tbmAlvo/peso;
+        frequenciaAlvo= (tmeModelo*frequencia)/tmeAlvo;
+        //antes de converter preciso tratar pra usar so 2 numeros depois do ponto.
+        String resultado = ("Posologia para o alvo é: " + posologiaAlvo + " e a frequencia é " + frequenciaAlvo);
+        return resultado;
+    }
+    public String calculo (int Kalvo , double peso, double posologia, int kmodelo, int frequencia, double concentra){
+
+        double tbmAlvo;
+        double tbmModelo;
+        double doseTotal;
+        double posologiaAlvo;
+        double frequenciaAlvo;
+        double tmeAlvo;
+        double tmeModelo;
+
+        tbmModelo = kmodelo*(Math.pow(10,0.75)); //  metabolismo do modelo = cao de 10kg
+        tbmAlvo = Kalvo*(Math.pow(peso,0.75)); // metabolismo  alvo que quer medicar.
+        doseTotal = ((posologia*10)/tbmModelo)*tbmAlvo; // dose total para o animal alvo
+        posologiaAlvo = doseTotal/peso; // para saber a posologia do animal alvo caso queira colocar no BD.
+        tmeModelo = tbmModelo/10;
+        tmeAlvo = tbmAlvo/peso;
+        frequenciaAlvo= (tmeModelo*frequencia)/tmeAlvo;
+        double doseTotalMl = doseTotal/concentra;
+
+        //antes de converter preciso tratar pra usar so 2 numeros depois do ponto.
+        String resultado = ("Dose total em é: "+ doseTotalMl +" ml, posologia é " + posologiaAlvo + " e a frequencia é " + frequenciaAlvo);
+        return resultado;
+
+    }
+    public String calculo (int Kalvo , double peso, double posologia, int kmodelo, double concentra) {
+
+        double tbmAlvo;
+        double tbmModelo;
+        double doseTotal;
+        double posologiaAlvo;
+
+        tbmModelo = kmodelo * (Math.pow(10, 0.75)); //  metabolismo do modelo = cao de 10kg
+        tbmAlvo = Kalvo * (Math.pow(peso, 0.75)); // metabolismo  alvo que quer medicar.
+        doseTotal = ((posologia * 10) / tbmModelo) * tbmAlvo; // dose total para o animal alvo
+        posologiaAlvo = doseTotal / peso; // para saber a posologia do animal alvo caso queira colocar no BD.
+        double doseTotalMl = doseTotal / concentra;
+
+        //antes de converter preciso tratar pra usar so 2 numeros depois do ponto.
+        String resultado = ("Dose total em é: "+ doseTotalMl +" ml, posologia é " + posologiaAlvo);
+        return resultado;
+    }
+
+
+}
